95b5b8108d0f2cda09168bb5b851a46df4fbd681
malek moslah <malek.moslahh@gmail.com>
2023-06-05 17:36:57 +0100

    create page accueil and page header

---
+++ Gestion/header.php
@@ -0,0 +1,225 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>CodePen - menu with awesome hover </title>
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css'>
+    <link rel='stylesheet' href='assets/css/bootstrap.min.css'>
+    <link href="https://fonts.cdnfonts.com/css/bambino-2" rel="stylesheet">
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css'>
+    <link rel="stylesheet" href="assets/css/header.css">
+
+</head>
+
+<body>
+    <style>
+    .svgicon {
+        height: 158px !important;
+        width: 175px !important;
+        top: -7px !important;
+    }
+    </style>
+    <!-- partial:index.partial.html -->
+    <main class="site-wrapper">
+        <div class="bg-image page-home  " style="display: grid; grid-template-columns: 15% 85%; background-image: url(assets/images/background_accueil.jpg);
+         background-position: center; background-size: cover; overflow:hidden;height: 100vh; background:#e9ecef;">
+
+            <div style="display: flex; flex-direction: column; align-items: center;">
+                <div class="hexagon-item">
+                    <div class="hex-item">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div class="hex-item">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <a class="hex-content" href="../profil.php">
+                        <span class="hex-content-inner">
+                            <span class="icon">
+                                <i class="fa fa-user" aria-hidden="true"></i>
+                            </span>
+                            <span class="title">Profil</span>
+                        </span>
+                        <svg viewBox="0 0 173.20508075688772 200" class="svgicon" version="1.1"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M86.60254037844386 0L173.20508075688772 50L173.20508075688772 150L86.60254037844386 200L0 150L0 50Z"
+                                fill="#1e2530"></path>
+                        </svg>
+                    </a>
+                </div>
+                <div class="hexagon-item">
+                    <div class="hex-item">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div class="hex-item">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <a class="hex-content" href="../profil.php">
+                        <span class="hex-content-inner">
+                            <span class="icon">
+                                <i class="fa fa-users" aria-hidden="true"></i>
+                            </span>
+                            <span class="title">Utilisateurs</span>
+                        </span>
+                        <svg viewBox="0 0 173.20508075688772 200" class="svgicon" version="1.1"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M86.60254037844386 0L173.20508075688772 50L173.20508075688772 150L86.60254037844386 200L0 150L0 50Z"
+                                fill="#1e2530"></path>
+                        </svg>
+                    </a>
+                </div>
+                <div class="hexagon-item">
+                    <div class="hex-item">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div class="hex-item">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <a class="hex-content" href="../profil.php">
+                        <span class="hex-content-inner">
+                            <span class="icon">
+                                <i class="fa fa-tasks" aria-hidden="true"></i>
+                            </span>
+                            <span class="title">Projets</span>
+                        </span>
+                        <svg viewBox="0 0 173.20508075688772 200" class="svgicon" version="1.1"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M86.60254037844386 0L173.20508075688772 50L173.20508075688772 150L86.60254037844386 200L0 150L0 50Z"
+                                fill="#1e2530"></path>
+                        </svg>
+                    </a>
+                </div>
+                <div class="hexagon-item">
+                    <div class="hex-item">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div class="hex-item">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <a class="hex-content" href="../profil.php">
+                        <span class="hex-content-inner">
+                            <span class="icon">
+                                <i class="fa fa-calendar" aria-hidden="true"></i>
+                            </span>
+                            <span class="title">Planning</span>
+                        </span>
+                        <svg viewBox="0 0 173.20508075688772 200" class="svgicon" version="1.1"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M86.60254037844386 0L173.20508075688772 50L173.20508075688772 150L86.60254037844386 200L0 150L0 50Z"
+                                fill="#1e2530"></path>
+                        </svg>
+                    </a>
+                </div>
+                <div class="hexagon-item">
+                    <div class="hex-item">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div class="hex-item">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <a class="hex-content" href="../profil.php">
+                        <span class="hex-content-inner">
+                            <span class="icon">
+                                <i class="fa fa-laptop" aria-hidden="true"></i>
+                            </span>
+                            <span class="title">Matériels</span>
+                        </span>
+                        <svg viewBox="0 0 173.20508075688772 200" class="svgicon" version="1.1"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M86.60254037844386 0L173.20508075688772 50L173.20508075688772 150L86.60254037844386 200L0 150L0 50Z"
+                                fill="#1e2530"></path>
+                        </svg>
+                    </a>
+                </div>
+                <div class="hexagon-item">
+                    <div class="hex-item">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div class="hex-item">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <a class="hex-content" href="../profil.php">
+                        <span class="hex-content-inner">
+                            <span class="icon">
+                                <i class="fa fa-database" aria-hidden="true"></i>
+                            </span>
+                            <span class="title">Stock</span>
+                        </span>
+                        <svg viewBox="0 0 173.20508075688772 200" class="svgicon" version="1.1"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M86.60254037844386 0L173.20508075688772 50L173.20508075688772 150L86.60254037844386 200L0 150L0 50Z"
+                                fill="#1e2530"></path>
+                        </svg>
+                    </a>
+                </div>
+                <div class="hexagon-item">
+                    <div class="hex-item">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div class="hex-item">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <a class="hex-content" href="../profil.php">
+                        <span class="hex-content-inner">
+                            <span class="icon">
+                                <i class="fa fa-envelope-open" aria-hidden="true"></i>
+                            </span>
+                            <span class="title">Contact</span>
+                        </span>
+                        <svg viewBox="0 0 173.20508075688772 200" class="svgicon" version="1.1"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M86.60254037844386 0L173.20508075688772 50L173.20508075688772 150L86.60254037844386 200L0 150L0 50Z"
+                                fill="#1e2530"></path>
+                        </svg>
+                    </a>
+                </div>
+            </div>
+            <style>
+            </style>
+            <div id=ContenuDroite style="display: grid; grid-template-rows: 13.5% 86.5%;">
+                <div id="tetepage"
+                    style="display: flex; justify-content: flex-start; align-items: center;    gap: 42px;">
+                    <a href="" class="circleiconsslink"> <img src="assets/images/avatar.jpg"
+                            class="rounded-circle shadow-4 circleiconss" style="width: 55px; height:55px;"
+                            alt="Avatar" /></a>
+                    <div id="notification">
+                        <span class="icon" style="text-align: center;">
+                            <i class="fa fa-bell" aria-hidden="true" style="color: white;height: 26px;width: 33px;"></i>
+                        </span>
+                    </div>
+                </div>
---
+++ profil.php
@@ -0,0 +1,11 @@
+<?php
+include 'Gestion/header.php'
+?>
+     <div id="contenu" style="color:black">
+            </div>
+        </div>
+        </div>
+    </main>
+    <!-- partial -->
+</body>
+<html>
